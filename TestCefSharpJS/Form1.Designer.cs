01b3b500b8a0200f9b242938c0a006412e79bd56
MartySVK <marty.svk@gmail.com>
2015-10-06 11:11:27 +0200

    upload

---
+++ TestCefSharpJS/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TestCefSharpJS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelChrome = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelChrome
+            // 
+            this.panelChrome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChrome.Location = new System.Drawing.Point(0, 0);
+            this.panelChrome.Name = "panelChrome";
+            this.panelChrome.Size = new System.Drawing.Size(800, 600);
+            this.panelChrome.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.panelChrome);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelChrome;
+    }
+}
+
---
+++ TestCefSharpJS/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace TestCefSharpJS
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CefSettings cefsettings = new CefSettings();
+            cefsettings.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0";
+            cefsettings.LogSeverity = LogSeverity.Disable;
+            cefsettings.CefCommandLineArgs.Add("enable-npapi", "1");
+            cefsettings.CefCommandLineArgs.Add("disable-bundled-ppapi-flash", "1");
+
+            Cef.Initialize(cefsettings);
+
+            chromeBrowser = new ChromiumWebBrowser("https://www.google.com/");
+            chromeBrowser.RegisterJsObject("jsObject", this, false);
+
+            chromeBrowser.Size = new Size(800, 600);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            this.panelChrome.Controls.Add(chromeBrowser);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            chromeBrowser.Dispose();
+            Cef.Shutdown();
+            Application.Exit();
+            //Close();
+        }
+    }
+}
